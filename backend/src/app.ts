cf5e210a34aae43e376b023f56973473f0ad1f07
Lee jeongchan <sjk338285@gmail.com>
2025-05-18 19:39:06 +0900

    feat : swagger 설치 및 설정파일 생성

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,14 +3,18 @@ import dotenv from "dotenv";
 import authRouter from "./routes/auth.route";
 import todoRouter from "./routes/todo.route";
 import teamRouter from "./routes/team.route";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./swagger";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use("/auth", authRouter);
-app.use("/", todoRouter);
-app.use("/", teamRouter);
+app.use("/todos", todoRouter);
+app.use("/teams", teamRouter);
 
 app.listen(PORT, () => {
   console.log(`서버 실행 : ${PORT}`);
---
+++ backend/src/swagger.ts
@@ -0,0 +1,18 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+export const swaggerSpec = swaggerJSDoc({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Todo API",
+      version: "1.0.0",
+      description: "Todo & Team API documentation using Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000", // .env 기반으로 추후 교체 가능 여부
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"], // 라우터 파일에 Swagger 주석 작성하면 여기서 읽는다.
+});
