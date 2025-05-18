3cb3507c9d1ea18fab5f1adfcfe4eb1d81671aa0
Lee jeongchan <sjk338285@gmail.com>
2025-05-18 19:26:23 +0900

    feat : app.ts 라우터 연결 및 테스트

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route";
+import todoRouter from "./routes/todo.route";
+import teamRouter from "./routes/team.route";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use("/auth", authRouter);
+app.use("/", todoRouter);
+app.use("/", teamRouter);
 
 app.listen(PORT, () => {
   console.log(`서버 실행 : ${PORT}`);
