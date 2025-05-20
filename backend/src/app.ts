8cabbc30c741912117887e658e0495c848d8ebbe
Lee jeongchan <sjk338285@gmail.com>
2025-05-20 17:39:42 +0900

    feat : json parser 누락 해결

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,6 +8,7 @@ import { swaggerSpec } from "./swagger";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
