69d25b460e259e6aa96283aed484b0380e10d1f0
Lee jeongchan <sjk338285@gmail.com>
2025-05-22 22:11:47 +0900

    refactor : http-code 라이브러리 사용하는 방식으로 리펙토링

--- backend/src/controller/todo.controller.ts
+++ backend/src/controller/todo.controller.ts
@@ -1,78 +1,75 @@
 import { Request, Response } from "express";
-import { createTodoService, getTodosService } from "../services/todo.service";
+import {
+  createTodoService,
+  getTodosService,
+  updateTodoContentsService,
+  toggleTodoDoneService,
+  deleteTodoService,
+} from "../services/todo.service";
 import { StatusCodes } from "http-status-codes";
-import { updateTodoContentsService } from "../services/todo.service";
-import { toggleTodoDoneService } from "../services/todo.service";
-import { deleteTodoService } from "../services/todo.service";
 
 export const createTodo = async (req: Request, res: Response) => {
   const userId = req.user?.id;
   const { contents } = req.body;
 
   if (!contents) {
-    res.status(StatusCodes.BAD_REQUEST).json({ message: "내용을 입력하세요." });
-    return;
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "내용을 입력하세요." });
   }
 
   const newTodo = await createTodoService(userId!, contents);
-  res.status(StatusCodes.CREATED).json(newTodo);
-  return;
+  return res.status(StatusCodes.CREATED).json(newTodo);
 };
 
 export const getTodos = async (req: Request, res: Response) => {
   const userId = req.user?.id;
 
   const todos = await getTodosService(userId!);
-  res.status(StatusCodes.OK).json(todos);
+  return res.status(StatusCodes.OK).json(todos);
 };
 
-export const updateTodoContents = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
+export const updateTodoContents = async (req: Request, res: Response) => {
   const userId = req.user?.id;
   const todoId = Number(req.params.id);
   const { contents } = req.body;
 
   if (!contents) {
-    res.status(400).json({ message: "내용을 입력하세요." });
-    return;
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "내용을 입력하세요." });
   }
 
   const updated = await updateTodoContentsService(todoId, userId!, contents);
-  res.status(200).json(updated);
+  return res.status(StatusCodes.OK).json(updated);
 };
 
-export const toggleTodoDone = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
+export const toggleTodoDone = async (req: Request, res: Response) => {
   const userId = req.user?.id;
   const todoId = Number(req.params.id);
   const { isDone } = req.body;
 
   if (typeof isDone !== "boolean") {
-    res.status(400).end();
-    return;
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "isDone 값이 잘못되었습니다." });
   }
 
   const updated = await toggleTodoDoneService(todoId, userId!, isDone);
-  res.status(200).json(updated);
+  return res.status(StatusCodes.OK).json(updated);
 };
 
-export const deleteTodo = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
+export const deleteTodo = async (req: Request, res: Response) => {
   const userId = req.user?.id;
   const todoId = Number(req.params.id);
 
   const deleted = await deleteTodoService(todoId, userId!);
 
   if (deleted.count === 0) {
-    res.status(404).json({ message: "삭제할 Todo를 찾을 수 없습니다." });
-    return;
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ message: "삭제할 Todo를 찾을 수 없습니다." });
   }
 
-  res.status(200).json({ message: "삭제 완료!" });
+  return res.status(StatusCodes.OK).json({ message: "삭제 완료!" });
 };
