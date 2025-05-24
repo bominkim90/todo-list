2e9b3edde68138e61b4362b45a6f57337788295a
bominkim90 <bomin97@naver.com>
2025-05-25 01:31:49 +0900

    feat: (front) 로그인시 화면 validation 틀리면 css 적용

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -20,10 +20,10 @@ function Login() {
       <div className={`input-wrap ${validationFail && "validation"}`}>
         <h1 className="text-align-center">로그인</h1>
         
-        <input id="user_id" className={`input ${validationFail && "validation"}`} type="text" placeholder="아이디를 입력해주세요."
+        <input id="user_id" className="input validation" type="text" placeholder="아이디를 입력해주세요."
           value={userId} onChange={ (event) => {setUserId(event.target.value); setValidationFail(false);} } />
         
-        <input id="user_pw" className={`input ${validationFail && "validation"}`} type="password" placeholder="비밀번호를 입력해주세요."
+        <input id="user_pw" className="input validation" type="password" placeholder="비밀번호를 입력해주세요."
           value={userPw} onChange={ (event) => {setUserPw(event.target.value); setValidationFail(false); } } />
         
         {validationFail && <p style={{'color':'#EF4444', 'fontSize':'14px','fontWeight':'500','lineHeight':'1.5'}}>아이디와 비밀번호를 확인해주세요.</p>}
